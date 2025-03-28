669dcb1d234c4abc9b33bc59b22b7fe5033222bd
dmitrykleymenov <dukefruitarian@gmail.com>
2025-03-28 16:31:09 +0300

    Added formatter settings

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,7 @@
 # Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  import_deps: [],
+  inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}"],
+  line_length: 120,
+  locals_without_parens: []
 ]
