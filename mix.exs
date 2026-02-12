fa62faf733ca3c3ca0bc111349f439bf51d143a8
dmitrykleymenov <dukefruitarian@gmail.com>
2026-02-12 10:07:46 +0300

    Fix library supported elixir version

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule ModuleResolver.MixProject do
     [
       app: :module_resolver,
       version: "0.1.0",
-      elixir: "~> 1.13.4",
+      elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       package: package(),
